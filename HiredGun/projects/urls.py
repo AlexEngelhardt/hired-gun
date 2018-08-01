39d5a42bd0c0e6574c6f76a7db182d20408ed542
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2018-08-01 18:26:54 +0200

    I can add projects (from the client list view) and delete projects (from the 'all projects' list)

--- HiredGun/projects/urls.py
+++ HiredGun/projects/urls.py
@@ -18,11 +18,13 @@ urlpatterns = [
     ## Clients
     path('clients/', views.ClientListView.as_view(), name='clients'),
     path('client/<int:pk>/', views.ClientDetailView.as_view(), name='client-detail'),
-
+    path('client/<int:client_id>/add_project', views.add_project, name='add-project'),
+    
     ## Projects
     path('projects/', views.ProjectListView.as_view(), name='projects'),
     path('project/<int:pk>/', views.ProjectDetailView.as_view(), name='project-detail'),
-
+    path('project/<int:client_id>/remove', views.remove_project, name='remove-project'),
+    
     ## Sessions
     path('sessions/', views.SessionListView.as_view(), name='sessions'),
     path('session/<int:pk>/', views.SessionDetailView.as_view(), name='session-detail'),
--- HiredGun/projects/views.py
+++ HiredGun/projects/views.py
@@ -1,9 +1,11 @@
 from django.views import generic
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 from .models import Client, Project, Session
 
- 
+
 #### Index
 
 def index(request):
@@ -26,6 +28,59 @@ class ProjectListView(generic.ListView):
 class ProjectDetailView(generic.DetailView):
     model = Project
     # template_name = 'projects/project_detail.html'
+
+def remove_project(request, client_id):
+    project = get_object_or_404(Project, pk=client_id)
+    the_projects_client = project.client
+    project.delete()
+    
+    return HttpResponseRedirect(reverse('projects:client-detail', args=(the_projects_client.id,)))
+    
+    
+def add_project(request, client_id):
+    try:
+        client = Client.objects.get(pk=client_id)
+    except Client.DoesNotExist:
+        raise Http404("Client does not exist :(")
+
+    # This is not pretty yet:
+    # If end_date is not supplied, the <form> passes '' instead of None.
+    # I need to replace it here.
+    end_date = request.POST.get('end_date')
+    if end_date == '':
+        end_date = None
+    
+    new_proj = Project(
+        name = request.POST.get('name', ''),
+        client = client,
+        rate = request.POST.get('rate', 0),
+        rate_unit = request.POST.get('rate_unit', 'hr'),
+        start_date = request.POST.get('start_date', None),
+        end_date = end_date
+    )
+
+    new_proj.save()
+
+    ## Do not do this!:
+    # template = loader.get_template('projects/client_detail.html')
+    # context = {
+    #     'client': client,
+    #     'message': 'Project added!',
+    # }
+    # return HttpResponse(template.render(context, request))
+
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+    return HttpResponseRedirect(reverse('projects:client-detail', args=(client.id,)))
+    # We are using the reverse() function in the HttpResponseRedirect constructor in
+    # this example. This function helps avoid having to hardcode a URL in the view
+    # function. It is given the name of the view that we want to pass control to and
+    # the variable portion of the URL pattern that points to that view. In this
+    # case, using the URLconf we set up in Tutorial 3, this reverse() call will return a string like
+    # '/polls/3/results/'
+
+    # TODO: But now I can't pass the "Project added!" message anymore :(
 
 
 #### Clients
