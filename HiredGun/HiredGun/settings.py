337746183f602c8c148aa01df0e8776d921a9726
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2018-08-02 15:41:18 +0200

    using template extensions now (base.html)

--- HiredGun/HiredGun/settings.py
+++ HiredGun/HiredGun/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-de'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
