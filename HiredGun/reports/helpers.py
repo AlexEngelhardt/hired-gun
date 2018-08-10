426ad337d8418ce797bab6790ea29ec0747ec70b
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2018-08-10 11:17:17 +0200

    clean up migrations, fix reports/view error when cloning fresh repo

--- HiredGun/reports/helpers.py
+++ HiredGun/reports/helpers.py
@@ -1,5 +1,6 @@
 import datetime
 from projects.models import Session
+from django.db.utils import OperationalError
 
 def get_initial_values():
     """
@@ -7,19 +8,39 @@ def get_initial_values():
     We check for the most recent ("youngest") session entered, 
     and the default values create a report for that entire month.
     """
-    
-    youngest_session = Session.objects.order_by('date').last()
-    youngest_date_with_sessions = youngest_session.date
 
-    first_of_that_month = youngest_date_with_sessions.replace(day=1)
-    last_of_that_month = (first_of_that_month.replace(day=28) + datetime.timedelta(days=4)).replace(day=1) - datetime.timedelta(days=1)
+    try: 
+        youngest_session = Session.objects.order_by('date').last()
+        youngest_date_with_sessions = youngest_session.date
+
+        first_of_that_month = youngest_date_with_sessions.replace(day=1)
+        last_of_that_month = (first_of_that_month.replace(day=28) + datetime.timedelta(days=4)).replace(day=1) - datetime.timedelta(days=1)
     
-    # TODO rename the variables to sth now meaningful. It's not "previous_month" anymore
-    context = {
-        'previous_month': first_of_that_month.month,
-        'year_of_previous_month': first_of_that_month.year,
-        'first_of_prev_month': first_of_that_month.strftime('%Y-%m-%d'),
-        'last_of_prev_month': last_of_that_month.strftime('%Y-%m-%d')
-    }
+        # TODO rename the variables to sth now meaningful. It's not "previous_month" anymore
+        context = {
+            'previous_month': first_of_that_month.month,
+            'year_of_previous_month': first_of_that_month.year,
+            'first_of_prev_month': first_of_that_month.strftime('%Y-%m-%d'),
+            'last_of_prev_month': last_of_that_month.strftime('%Y-%m-%d')
+        }
+    except OperationalError:
+        # If you just pulled this repository and your DB is fresh,
+        # trying to access the DB in a view throws an error.
+        # Catch it and fall back to displaying "last month" here.
+        # See:
+        # https://stackoverflow.com/questions/34548768/django-no-such-table-exception/36453000
+        
+        last_of_prev_month = datetime.date.today().replace(day=1) - datetime.timedelta(days=1)
+        first_of_prev_month = last_of_prev_month.replace(day=1)
+        previous_month = last_of_prev_month.month
+        year_of_previous_month = last_of_prev_month.year
+
+        context = {
+            'previous_month': previous_month,
+            'year_of_previous_month': year_of_previous_month,
+            'first_of_prev_month': first_of_prev_month.strftime('%Y-%m-%d'),
+            'last_of_prev_month': last_of_prev_month.strftime('%Y-%m-%d')
+        }
+        
     return context
         
