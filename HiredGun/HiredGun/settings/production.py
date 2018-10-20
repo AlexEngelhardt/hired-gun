bc038c3f98a1be1d6d7eec8f0ee78bb03a6253df
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2018-10-20 19:32:31 +0200

    debug prod.py

--- HiredGun/HiredGun/settings/production.py
+++ HiredGun/HiredGun/settings/production.py
@@ -5,7 +5,7 @@ from HiredGun.settings.common import *
 # To run with production instead of development settings,
 # set the DJANGO_SETTINGS_MODULE environment variable to 'HiredGun.settings.production'
 
-DEBUG = False
+DEBUG = True
 
 # Generate a secret.txt by:
 # head -c 256 /dev/urandom | md5sum | cut -f 1 -d\ > secret.txt
